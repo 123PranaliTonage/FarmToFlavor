27690c8c456243e10f283e68995b690cd6296c67
Pranali Tonage <pranalitonage227@gmail.com>
2026-03-03 17:54:04 +0530

    Corrected PORT usage for deployment

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.post("/api/enquiry", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+ const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
